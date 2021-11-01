6aab72d7c033ea2d7170dbabe5555b71720c5b7c
KDY <rlaeodud3687@naver.com>
2021-11-01 14:07:27 +0900

    GoogleMap Clusturing [마커가 모여있으면 10+ 이런방식으로 모아서 보여주는 기능 작성]

--- app/src/main/java/org/gsm/software/eodigasszip/view/activity/MapsActivity.kt
+++ app/src/main/java/org/gsm/software/eodigasszip/view/activity/MapsActivity.kt
@@ -9,41 +9,66 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.maps.android.clustering.ClusterManager
 import org.gsm.software.eodigasszip.R
 import org.gsm.software.eodigasszip.databinding.ActivityMapsBinding
+import org.gsm.software.eodigasszip.model.ClusterRenderer
+import org.gsm.software.eodigasszip.model.MyItem
+import org.gsm.software.eodigasszip.viewmodel.MapViewModel
+import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import kotlin.math.ln
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    var clusterManager: ClusterManager<MyItem>? = null
+    val viewModel : MapViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMapsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.maps = this
 
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        val gsm = LatLng(35.143652150335576, 126.80023535027976)
-        // Add a marker in Sydney and move the camera
-        mMap.addMarker(MarkerOptions().position(gsm).title("광주소프트웨어마이스터고"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(gsm))
+        //초기 지도 위치 세팅
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(35.1429503,126.8005143),5.0f))
+        clusterManager = ClusterManager(this,mMap)
+        mMap.setOnCameraIdleListener(clusterManager)
+        mMap.setOnMarkerClickListener(clusterManager)
+        addMaker()
+    }
+
+    private fun addMaker(){
+        var lat = 35.1429503
+        var lng = 126.8005143
+        var title = "t"
+        var sni = "s"
+
+        for (i in 0..9) {
+            val offset = i / 30.0
+            lat += offset
+            lng += offset
+            title = title+"$i"
+            sni = sni+"$i"
+            val offsetItem =
+                MyItem(LatLng(lat, lng),title,sni)
+            clusterManager?.addItem(offsetItem)
+        }
+        clusterManager?.cluster()
 
     }
+
+
 }
